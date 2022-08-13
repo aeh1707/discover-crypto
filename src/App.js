fdb54dffdeb4524a8971bef023180be6b4df4176
aeh1707d <abdelhadi.hireche@outlook.com>
2022-08-13 15:08:18 +0100

    Configure router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import TopCoins from './components/TopCoins';
+import CoinInfo from './components/CoinInfo';
 
 function App() {
   return (
-    <div className="App">
-      hello world
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<TopCoins />} />
+          <Route path="/coininfo/:id" element={<CoinInfo />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
